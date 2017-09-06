c599cc964ce05e0718954358dd48fe9e63fa0ed9
Gautam Dhameja <gautamdh@outlook.com>
2017-09-06 15:26:59 +0200

    updated link validation

--- bigchaindb_smart_assets/consensus.py
+++ bigchaindb_smart_assets/consensus.py
@@ -1,3 +1,5 @@
+import logging
+
 from bigchaindb.common.exceptions import ValidationError
 from bigchaindb.consensus import BaseConsensusRules
 from bigchaindb.models import Transaction
@@ -9,6 +11,7 @@ ASSET_RULE_ROLE = 'role'
 ASSET_RULE_LINK = 'link'
 METADATA_RULE_CAN_LINK = 'canLink'
 
+logger = logging.getLogger(__name__)
 
 class SmartAssetConsensusRules(BaseConsensusRules):
 
@@ -88,7 +91,10 @@ class SmartAssetConsensusRules(BaseConsensusRules):
 
     @staticmethod
     def validate_link(transaction, bigchain):
+        logger.info('Validating link')
         public_key = transaction.inputs[0].owners_before[0]
+        logger.info('Owner: %s', public_key)
+
         cant_link_error = 'Linking is not authorized for: {}'.format(public_key)
 
         if transaction.operation == Transaction.GENESIS or\
@@ -103,25 +109,50 @@ class SmartAssetConsensusRules(BaseConsensusRules):
             return
 
         link = transaction.asset['data']['link']
+        logger.info('Link: %s', link)
         tx_to_link = bigchain.get_transaction(link)
-        
+
+        if not tx_to_link:
+            raise ValidationError('Transaction to link not found {}'
+                                    .format(link))
+
+        logger.info('Link Transaction: %s', tx_to_link.id)
+
         if tx_to_link and not hasattr(tx_to_link, 'metadata'):
-            raise ValidationError(cant_link_error)
-        
+            raise ValidationError('Metadata not found in transaction {}'
+                                    .format(tx_to_link))
+
         if tx_to_link.metadata is None or METADATA_RULE_CAN_LINK not in tx_to_link.metadata:
-            raise ValidationError(cant_link_error)
+            raise ValidationError('canLink not found in metadata of transaction {}'
+                                    .format(tx_to_link))
 
         can_link = tx_to_link.metadata[METADATA_RULE_CAN_LINK]
-        wallet_tx = bigchain.get_owned_ids(public_key)
-        wallet_tx_ids = [tx.txid for tx in wallet_tx]
-        wallet_tx_assets = bigchain.get_assets(wallet_tx_ids)
+        logger.info('Can link: %s', can_link)
 
-        if wallet_tx_assets:
-            role_asset_tx = [asset for asset in wallet_tx_assets if asset['data']['link'] and\
-             asset['data']['link'] == can_link]
+        if isinstance(can_link, list):
+            logger.info('canLink is a list, looking up for public key of owner')
+            if public_key in can_link:
+                logger.info('Link valid: public key found in canLink')
+                return
+            else:
+                raise ValidationError(cant_link_error)
+        elif isinstance(can_link, str):
+            logger.info('canLink is a string, looking up assets in owner wallet')
+            wallet_tx = bigchain.get_owned_ids(public_key)
+            wallet_tx_ids = [tx.txid for tx in wallet_tx]
 
-        if not role_asset_tx:
-            raise ValidationError(cant_link_error)
+            for asset_id in wallet_tx_ids:
+                asset = bigchain.get_transaction(asset_id).asset
+                if asset and asset['data'] and ASSET_RULE_LINK in asset['data']:
+                    if asset['data']['link'] == can_link:
+                        logger.info('Link valid: asset link is equal to canLink')
+                        break
+                else:
+                    continue
+            else:
+                raise ValidationError(cant_link_error)
+        else:
+            raise ValidationError('canLink is not valid')
 
         return
 
