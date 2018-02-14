81913afe6a33c3a7b66c2f7227d04efd7e9d9613
Gautam Dhameja <gautamdhameja@users.noreply.github.com>
2018-02-14 23:27:27 +0100

    Revert "added support for list of can_link asset ids"
    This reverts commit 7f23ac9fb8e1505e4c0313d5bf134d047801d70c.

--- bigchaindb_smart_assets/consensus.py
+++ bigchaindb_smart_assets/consensus.py
@@ -92,9 +92,11 @@ class SmartAssetConsensusRules(BaseConsensusRules):
         logger.info('Validating link')
         public_key = transaction.inputs[0].owners_before[0]
 
+        cant_link_error = 'Linking is not authorized for: {}'.format(public_key)
+
         # Dont't do anything when it's GENESIS or TRANSFER transaction
         if transaction.operation == Transaction.GENESIS or\
-                transaction.operation == Transaction.TRANSFER:
+            transaction.operation == Transaction.TRANSFER:
             return
 
         if not hasattr(transaction, 'asset'):
@@ -111,47 +113,59 @@ class SmartAssetConsensusRules(BaseConsensusRules):
 
         if not tx_to_link:
             raise ValidationError('Transaction not resolved to link: {}'
-                                  .format(link))
+                                    .format(link))
 
         logger.info('Link Transaction: %s', tx_to_link.id)
 
         if tx_to_link and not hasattr(tx_to_link, 'metadata'):
             raise ValidationError('Metadata not found in transaction {}'
-                                  .format(tx_to_link))
+                                    .format(tx_to_link))
 
         if tx_to_link.metadata is None or METADATA_RULE_CAN_LINK not in tx_to_link.metadata:
             raise ValidationError('can_link not found in metadata of transaction {}'
-                                  .format(tx_to_link))
+                                    .format(tx_to_link))
 
         can_link = tx_to_link.metadata[METADATA_RULE_CAN_LINK]
         logger.info('Can link: %s', can_link)
 
         # can_link validation
-        # if can_link is a list
-        # check if can_link is a list of transaction ids or public keys
-        # check if the public key of the user is a part of it or not
-        # OR
-        # check if the user has a premission asset linked to the can_link asset
+        # if can_link is a list,
+        # it means we need to check if the public key of the user is a part of it or not
         if isinstance(can_link, list):
-            logger.info('can_link is a list')
-            if SmartAssetConsensusRules.check_if_transaction_id(bigchain, can_link[0]):
-                SmartAssetConsensusRules\
-                .validate_can_link(bigchain, can_link, public_key)
+            logger.info('can_link is a list, looking up for public key of owner')
+            if public_key in can_link:
+                logger.info('Link valid: public key in can_link')
+                return
             else:
-                if public_key in can_link:
-                    logger.info('Link valid: public key in can_link')
-                    return
-                else:
-                    raise ValidationError('Linking is not authorized for: {}'.format(
-                        public_key))
-        # backward compatibility - if can_link is string then convert it to a list
+                raise ValidationError(cant_link_error)
+
+        # else if can_link is a string
+        # it means we need to check if the can_link value is a valid asset id and
+        # if the user has a premission asset linked to the can_link asset
         elif isinstance(can_link, str):
-            logger.info('can_link is a string')
-            can_link_list = [can_link]
-            SmartAssetConsensusRules\
-            .validate_can_link(bigchain, can_link_list, public_key)
+            logger.info('can_link is a string, looking up assets in owner wallet')
+            wallet_tx = bigchain.get_owned_ids(public_key)
+            wallet_tx_ids = [tx.txid for tx in wallet_tx]
+            logger.info('Wallet has %s assets', len(wallet_tx_ids))
+
+            for asset_id in wallet_tx_ids:
+                logger.info('Looking up asset: %s', asset_id)
+                trans = bigchain.get_transaction(asset_id)
+                permission_asset = trans.asset
+                if trans.operation == Transaction.TRANSFER:
+                    permission_asset = bigchain.get_transaction(trans.asset['id']).asset
+                if permission_asset and permission_asset['data'] and\
+                    ASSET_RULE_LINK in permission_asset['data']:
+                    if permission_asset['data']['link'] == can_link:
+                        logger.info('Link valid: asset.link is can_link')
+                        break
+                else:
+                    continue
+            else:
+                raise ValidationError(cant_link_error)
         else:
             raise ValidationError('can_link is not valid')
+
         return
 
     @staticmethod
@@ -181,46 +195,3 @@ class SmartAssetConsensusRules(BaseConsensusRules):
             return [bigchain.get_transaction(asset_id).asset
                     for asset_id
                     in asset_ids]
-
-    @staticmethod
-    def validate_can_link(bigchain, can_link, public_key):
-        logger.info('validating can_link, looking up assets in owner wallet')
-        wallet_tx = bigchain.get_owned_ids(public_key)
-        wallet_tx_ids = [tx.txid for tx in wallet_tx]
-        logger.info('Wallet has %s assets', len(wallet_tx_ids))
-
-        for asset_id in wallet_tx_ids:
-            logger.info('Looking up asset: %s', asset_id)
-            trans = bigchain.get_transaction(asset_id)
-            if trans.operation == Transaction.TRANSFER:
-                permission_asset = bigchain.get_transaction(
-                    trans.asset['id']).asset
-            else:
-                permission_asset = trans.asset
-            if permission_asset and permission_asset['data'] and\
-                    ASSET_RULE_LINK in permission_asset['data']:
-                if permission_asset['data']['link'] in can_link:
-                    logger.info('Link valid: asset.link found in can_link')
-                    break
-            else:
-                continue
-        else:
-            raise ValidationError('Linking is not authorized for: {}'.format(
-                        public_key))
-        return
-
-    @staticmethod
-    def check_if_transaction_id(bigchain, transaction_id):
-        logger.info('Checking if tx id: {}'.format(transaction_id))
-        is_tx_id = True
-        try:
-            tx = bigchain.get_transaction(transaction_id)
-            if tx:
-                logger.info('Tx id check passed for: {}'.format(transaction_id))
-            else:
-                logger.info('Tx id check failed for: {}'.format(transaction_id))
-                is_tx_id = False
-        except:
-            logger.info('Tx id check failed for: {}'.format(transaction_id))
-            is_tx_id = False
-        return is_tx_id
